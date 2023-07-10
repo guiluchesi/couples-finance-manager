0937ca64c93dab9dd38f81de56d1c0297d7c511b
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2023-07-10 00:00:59 -0300

    chore(app): connect app to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.DATA_PORT),
+      username: process.env.DATA_BASE_USER,
+      password: process.env.DATA_BASE_PASSWORD,
+      database: process.env.DATA_BASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
