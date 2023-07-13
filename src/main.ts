bf2809e19217b0c5dbe6e6bc29cb402c6a52431b
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2023-07-13 15:21:16 -0300

    fix(app): use port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const DEFAULT_APPLICATION_PORT = 3000;
+const APPLICATION_PORT = process.env.PORT || DEFAULT_APPLICATION_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APPLICATION_PORT);
+  console.log(`Application running on port ${APPLICATION_PORT}`);
 }
 bootstrap();
