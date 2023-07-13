b0ed86975624987bca1ae885b949260fee4155cf
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2023-07-13 13:42:29 -0300

    fix(db): use correct env for db port

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATA_BASE_HOST,
-      port: Number(process.env.DATA_PORT),
+      port: Number(process.env.DATA_BASE_PORT),
       username: process.env.DATA_BASE_USER,
       password: process.env.DATA_BASE_PASSWORD,
       database: process.env.DATA_BASE_NAME,
